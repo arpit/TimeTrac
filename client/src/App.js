07f1f74cfb8b65bc8910fa75a70cc9055f7add8e
Arpit Mathur <mathur.arpit@gmail.com>
2019-01-07 12:13:26 -0500

    added router and a track class

--- client/src/App.js
+++ client/src/App.js
@@ -1,108 +1,57 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Moment from 'react-moment';
-import moment from 'moment'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-const days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"]
 
+import WeekView from './components/week_view'
+import NewTrackForm from './components/new_track_form'
 
 class App extends Component {
 
   constructor(){
       super()
-      this.state = { weekOffset:0, tracks: []}
-  }
-
-  getWeekDates(offset){
-    var curr = new Date(); // get current date
-    var first = curr.getDate() - curr.getDay() + offset*7; // First day is the day of the month - the day of the week
-    var last = first + 6; // last day is the first day + 6
-    return {first: first, last: last}
-  }
-
-  getWeek(offset){
-    var curr = new Date();
-    let dates = this.getWeekDates(offset)
-    var firstday = new Date(curr.setDate(dates.first)).toUTCString();
-    var lastday = new Date(curr.setDate(dates.last)).toUTCString();
-    return { start: firstday, end: lastday }
-  }
-
-  componentDidMount(){
-    this.setState({weekOffset:0})
-    this.loadDataFor(0)
-  }
-
-  trackDataToView(item){
-    let cells = days.map( (day)=>{
-      return <td className="" key={item.name+"_"+day}><input type="text" name={item.name+"_"+day}/></td>
-    } )
-
-    return <tr key={item.name+"_row"} className="Track-Row">
-            <td key={item.name+"_trac"}>{item.name}</td>
-            {cells}
-            </tr>
-  }
-
-
-  changeWeek(increment){
-    let nowWeek = this.state.weekOffset + increment;
-    this.setState({weekOffset: nowWeek})
-    this.loadDataFor( nowWeek )
-  }
-
-  loadDataFor(week){
-    let start = this.getWeek(week).start;
-    let m = moment(start).format("YYYY-MM-DD")
-  }
-
-  saveDataFor(week){
+      this.onNewTrackClicked = this.onNewTrackClicked.bind(this)
 
   }
 
+  onNewTrackClicked(event){
 
+  }
+
+  renderMergedProps(component, ...rest){
+    const finalProps = Object.assign({}, ...rest);
+    return (
+      React.createElement(component, finalProps)
+    );
+  }
 
   render() {
-
-    let nav = (
-                  <div className="Main-Controls">
-                    <div className="date_header">
-                    <span className="date_page_button date_left" onClick={ ()=>{ this.changeWeek(-1) }}> &laquo;</span>
-                    <Moment format="MMM DD">{this.getWeek(this.state.weekOffset).start}</Moment>&nbsp;-&nbsp;
-                    <Moment format="MMM DD">{this.getWeek(this.state.weekOffset).end}</Moment>
-                    <span className="date_page_button date_right"
-                          onClick={ ()=>{ this.changeWeek(1) }}>&raquo;</span>
-                    </div>
-                    <a href="#" className="Button save">Save</a>
-                    <a href="#" className="Button new-track-button">New Track</a>
-
-                  </div>
-                 )
-    let header_cells = days.map( (day)=>{
-      return <th width="8%" key={"header_"+day}>{day}</th>
-    } )
-    let table_header = <tr>
-                          <th width="40%" key="header_track">Track</th>
-                          {header_cells}
-                          </tr>
+    /*
+    React Router by default doesn't let you pass props to
+    route components
+    */
+    const PropsRoute = ({ component, ...rest }) => {
+      return (
+        <Route {...rest} render={routeProps => {
+          return this.renderMergedProps(component, routeProps, rest);
+        }}/>
+      );
+    }
 
     return (
       <div className="App">
         <div className="App-header">
+        </div>
 
-        </div>
-        <div className="App-content">
-          {nav}
-          <table width="100%" className="tracks" >
-            <thead>
-              {table_header}
-            </thead>
-            <tbody>
-              {this.state.tracks.map(this.trackDataToView)}
-            </tbody>
-          </table>
-        </div>
+        <Router>
+          <div>
+            <div className="App-content">
+            <PropsRoute path="/" exact component={WeekView} newTrackListener={this.onNewTrackClicked}/>
+            <Route path="/new/" component={NewTrackForm} />
+            </div>
+          </div>
+        </Router>
       </div>
     );
   }
---
+++ client/src/components/week_view.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import Moment from 'react-moment';
+import moment from 'moment'
+import Track from '../models/track'
+
+const days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"]
+
+
+class WeekView extends Component{
+
+  constructor(props){
+    super(props)
+    let track = new Track("Jellos")
+
+    this.state = { weekOffset:0, tracks: [track]}
+  }
+
+  componentDidMount(){
+    this.setState({weekOffset:0})
+    this.loadDataFor(0)
+  }
+
+  trackDataToView(item){
+    console.log("item::: ",item)
+    let cells = days.map( (day)=>{
+      return <td className="" key={item.name+"_"+day}><input type="text" name={item.name+"_"+day}/></td>
+    } )
+
+    return <tr key={item.name+"_row"} className="Track-Row">
+            <td key={item.name+"_trac"}>{item.name}</td>
+            {cells}
+            </tr>
+  }
+
+
+  changeWeek(increment){
+    let nowWeek = this.state.weekOffset + increment;
+    this.setState({weekOffset: nowWeek})
+    this.loadDataFor( nowWeek )
+  }
+
+
+
+  saveDataFor(week){
+
+  }
+
+
+  getWeekDates(offset){
+    var curr = new Date(); // get current date
+    var first = curr.getDate() - curr.getDay() + offset*7; // First day is the day of the month - the day of the week
+    var last = first + 6; // last day is the first day + 6
+    return {first: first, last: last}
+  }
+
+  getWeek(offset){
+    var curr = new Date();
+    let dates = this.getWeekDates(offset)
+    var firstday = new Date(curr.setDate(dates.first)).toUTCString();
+    var lastday = new Date(curr.setDate(dates.last)).toUTCString();
+    return { start: firstday, end: lastday }
+  }
+
+  loadDataFor(week){
+    let start = this.getWeek(week).start;
+    let m = moment(start).format("YYYY-MM-DD")
+  }
+
+  render(){
+
+    let nav = (
+                  <div className="Main-Controls">
+                    <div className="date_header">
+                    <span className="date_page_button date_left" onClick={ ()=>{ this.changeWeek(-1) }}> &laquo;</span>
+                    <Moment format="MMM DD">{this.getWeek(this.state.weekOffset).start}</Moment>&nbsp;-&nbsp;
+                    <Moment format="MMM DD">{this.getWeek(this.state.weekOffset).end}</Moment>
+                    <span className="date_page_button date_right"
+                          onClick={ ()=>{ this.changeWeek(1) }}>&raquo;</span>
+                    </div>
+                    <a href="#" className="Button save">Save</a>
+                    <a href="#" className="Button new-track-button"
+                        onClick={(e)=>{
+                          e.preventDefault();
+                          this.props.history.push("/new/");}}>New Track</a>
+
+                  </div>
+                 )
+    let header_cells = days.map( (day)=>{
+      return <th width="8%" key={"header_"+day}>{day}</th>
+    } )
+    let table_header = <tr>
+                          <th width="40%" key="header_track">Track</th>
+                          {header_cells}
+                          </tr>
+
+    return(
+      <div>
+      {nav}
+      <table width="100%" className="tracks" >
+        <thead>
+          {table_header}
+        </thead>
+        <tbody>
+          {this.state.tracks.map(this.trackDataToView)}
+        </tbody>
+      </table>
+      </div>
+    )
+  }
+}
+
+export default WeekView;
